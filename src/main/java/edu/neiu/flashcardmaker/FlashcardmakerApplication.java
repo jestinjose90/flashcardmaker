89e3bd5965fe1228bc0804cd70c67e7cc49d5ad1
Jestin Jose <fjestinjose@neiu.edu>
2021-09-28 17:59:23 -0500

    Initial commit

---
+++ src/main/java/edu/neiu/flashcardmaker/FlashcardmakerApplication.java
@@ -0,0 +1,14 @@
+package edu.neiu.flashcardmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardmakerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FlashcardmakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/neiu/flashcardmaker/FlashcardmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neiu.flashcardmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardmakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
