bfe4013af8cf1f07ffb90f3bd8bc4c52f1d27379
Jestin Jose <fjestinjose@neiu.edu>
2021-11-21 19:30:53 -0600

    updated

---
+++ src/main/java/edu/neiu/flashcardmaker/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package edu.neiu.flashcardmaker.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected  void configure(HttpSecurity http) throws  Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/flashcard/**").authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+
+    @Override
+    protected  void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER");
+
+    }
+
+}
